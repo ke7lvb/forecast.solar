91a4a119eb8bc532df183855be0fd756c547f560
ke7lvb <67347096+ke7lvb@users.noreply.github.com>
2021-10-26 15:53:09 -0600

    Update forecast.solar.groovy

--- forecast.solar.groovy
+++ forecast.solar.groovy
@@ -1,44 +1,47 @@
 metadata {
-	definition(
-name: "Forecast.Solar",
-namespace: "ke7lvb",
-author: "Ryan Lundell",
-	){
-		capability "Refresh"
-		capability "PowerMeter"
-        		
-		attribute "estimatedWattHoursToday","number"
-        attribute "estimatedWattHoursTomorrow","number"
-	}
-	preferences {
-		input name: "logEnable", type: "bool", title: "Enable Info logging", defaultValue: true, description: ""
-        input name: "lat", type: "string", title: "Latitude", description: "", required: true
-        input name: "lng", type: "string", title: "Longitude", description: "", required: true
-        input name: "dec", type: "string", title: "Declination", description: "0-90", required: true
-        input name: "az", type: "string", title: "Azimuth", description: "-180 - 180", required: true
-        input name: "kwp", type: "string", title: "Kilowatt Production", description: "", required: true
-        input("refresh_interval", "enum", title: "How often to refresh the battery data", options: [
-            0: "Do NOT update",
-            1: "1 Hour",
-            3: "3 Hours",
-            8: "8 Hours",
-            12: "12 Hours",
-            24: "Daily",
+  definition(
+    name: "Forecast.Solar",
+    namespace: "ke7lvb",
+    author: "Ryan Lundell",
+    importUrl: "https://raw.githubusercontent.com/ke7lvb/forecast.solar/main/forecast.solar.groovy",
+  ) {
+    capability "Refresh"
+    capability "PowerMeter"
+
+    attribute "estimatedWattHoursToday", "number"
+    attribute "estimatedWattHoursTomorrow", "number"
+  }
+  preferences {
+    input name: "logEnable", type: "bool", title: "Enable Info logging", defaultValue: true, description: ""
+    input name: "lat", type: "string", title: "Latitude", description: "", required: true
+    input name: "lng", type: "string", title: "Longitude", description: "", required: true
+    input name: "dec", type: "string", title: "Declination", description: "0-90", required: true
+    input name: "az", type: "string", title: "Azimuth", description: "", required: true
+    input name: "kwp", type: "string", title: "Kilowatt Production", description: "", required: true
+    input("refresh_interval", "enum", title: "How often to refresh the battery data", options: [
+      0: "Do NOT update",
+      1: "1 Hour",
+      3: "3 Hours",
+      8: "8 Hours",
+      12: "12 Hours",
+      24: "Daily",
     ], required: true, defaultValue: "8")
-	}
+  }
 }
 
-def version(){ return "1.0.0" }
+def version() {
+  return "1.0.0"
+}
 
-def installed(){
-	if(logEnable) log.info "Driver installed"
-	
-	state.version = version()
+def installed() {
+  if (logEnable) log.info "Driver installed"
+
+  state.version = version()
 }
 
 def uninstalled() {
-	unschedule(refresh)
-	if(logEnable) log.info "Driver uninstalled"
+  unschedule(refresh)
+  if (logEnable) log.info "Driver uninstalled"
 }
 
 def updated() {
@@ -54,18 +57,18 @@ def updated() {
   }
   state.version = version()
 }
-import groovy.json.JsonOutput;
+
 def refresh() {
-    today = new Date().format( 'yyyy-MM-dd' )
-    tomorrow = new Date().next().format("yyyy-MM-dd")
-    host = "https://api.forecast.solar/estimate/${lat}/${lng}/${dec}/${az}/${kwp}"
-    httpGet([uri: host]) {
-        resp -> def respData = resp.data.result
-        state.estimatedWattHoursToday = respData.watt_hours_day[today]
-        sendEvent(name: "power", value: state.estimatedWattHoursToday)
-        state.estimatedWattHoursTomorrow = respData.watt_hours_day[tomorrow]
-        sendEvent(name: "estimatedWattHoursTomorrow", value: state.estimatedWattHoursTomorrow)
-        state.JSON = JsonOutput.toJson(resp.data)
-        state.lastUpdate = new Date()
-    }
+  today = new Date().format('yyyy-MM-dd')
+  tomorrow = new Date().next().format("yyyy-MM-dd")
+  host = "https://api.forecast.solar/estimate/${lat}/${lng}/${dec}/${az}/${kwp}"
+  httpGet([uri: host]) {
+    resp -> def respData = resp.data.result
+    state.estimatedWattHoursToday = respData.watt_hours_day[today]
+    sendEvent(name: "power", value: state.estimatedWattHoursToday)
+    state.estimatedWattHoursTomorrow = respData.watt_hours_day[tomorrow]
+    sendEvent(name: "estimatedWattHoursTomorrow", value: state.estimatedWattHoursTomorrow)
+    state.JSON = resp.data
+    state.lastUpdate = new Date()
+  }
 }
